10e90c32ceb5429224756e468fc1ffd40651a298
vladnewbox <vladnewbox@gmail.com>
2026-09-04 16:39:49 +0300

    Add new file in add layer with only one generic with 3 methods

--- internal/app/repository.go
+++ internal/app/repository.go
@@ -3,7 +3,7 @@ package app
 import "rental/internal/domain"
 
 type Repository[T domain.Entity] interface {
-	SaveItem(item T) error
+	Save(item T) error
 	FindByID(id domain.ID) (T, error)
 	List() ([]T, error)
 }
