9ca6a7bcf24776fdc943e486524cd2f8777aa255
vladnewbox <vladnewbox@gmail.com>
2026-09-04 15:13:04 +0300

    Added new Entity interface

--- internal/domain/entity.go
+++ internal/domain/entity.go
@@ -4,7 +4,15 @@ import "time"
 
 type ID string
 
+type Entity interface {
+	GetID() ID
+}
+
 type BaseEntity struct {
 	ID        ID
 	CreatedAt time.Time
+}
+
+func (e BaseEntity) GetID() ID {
+	return e.ID
 }
