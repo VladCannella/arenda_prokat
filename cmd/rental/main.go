06629f2a0fb654717d1277a3d6ed2da07addf612
vladnewbox <vladnewbox@gmail.com>
2026-09-23 12:30:50 +0300

    Added printError and logic of printing errors

--- cmd/rental/main.go
+++ cmd/rental/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -56,65 +57,73 @@ func main() {
 			continue
 		}
 		fields := strings.Fields(line)
-		switch fields[0] {
-		case "exit":
+		if fields[0] == "exit" {
 			return
-		case "rent":
-			if len(fields) != 4 {
-				fmt.Println("usage: rent <itemID> <customerID> <days>")
-				continue
-			}
-			days, err := strconv.Atoi(fields[3])
-			if err != nil {
-				fmt.Println("invalid days: ", err)
-				continue
-			}
-			itemID := domain.ID(fields[1])
-			customerID := domain.ID(fields[2])
-			start := time.Now()
-			end := start.AddDate(0, 0, days)
-
-			rental, err := service.RentItem(itemID, customerID, start, end)
-			if err != nil {
-				fmt.Println("error: ", err)
-				continue
-			}
-			fmt.Println("rented, rental id: ", rental.ID)
-		case "return":
-			if len(fields) != 2 {
-				fmt.Println("usage: rent <rentalID>")
-				continue
-			}
-			rentalID := domain.ID(fields[1])
-			fine, err := service.ReturnItem(rentalID, time.Now())
-			if err != nil {
-				fmt.Println("error: ", err)
-				continue
-			}
-
-			fmt.Println("returned, rental id: ", rentalID, "fine: ", fine)
-		case "list":
-			items, err := itemRepo.List()
-			if err != nil {
-				fmt.Println("error: ", err)
-				continue
-			}
-
-			for _, it := range items {
-				fmt.Println(it.ID, it.Name, "-", itemStatusLabel(it.Status))
-			}
-			rentals, err := rentalRepo.List()
-			if err != nil {
-				fmt.Println("error: ", err)
-				continue
-			}
-
-			for _, r := range rentals {
-				fmt.Println(r.ID, "item: ", r.ItemID, "customer: ", r.CustomerID)
-			}
-		default:
-			fmt.Println("unknown command: ", fields[0])
 		}
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					fmt.Println("internal error", r)
+				}
+			}()
+			switch fields[0] {
+			case "rent":
+				if len(fields) != 4 {
+					fmt.Println("usage: rent <itemID> <customerID> <days>")
+					return
+				}
+				days, err := strconv.Atoi(fields[3])
+				if err != nil {
+					fmt.Println("invalid days: ", err)
+					return
+				}
+				itemID := domain.ID(fields[1])
+				customerID := domain.ID(fields[2])
+				start := time.Now()
+				end := start.AddDate(0, 0, days)
+
+				rental, err := service.RentItem(itemID, customerID, start, end)
+				if err != nil {
+					printError(err)
+					return
+				}
+				fmt.Println("rented, rental id: ", rental.ID)
+			case "return":
+				if len(fields) != 2 {
+					fmt.Println("usage: rent <rentalID>")
+					return
+				}
+				rentalID := domain.ID(fields[1])
+				fine, err := service.ReturnItem(rentalID, time.Now())
+				if err != nil {
+					printError(err)
+					return
+				}
+
+				fmt.Println("returned, rental id: ", rentalID, "fine: ", fine)
+			case "list":
+				items, err := itemRepo.List()
+				if err != nil {
+					printError(err)
+					return
+				}
+
+				for _, it := range items {
+					fmt.Println(it.ID, it.Name, "-", itemStatusLabel(it.Status))
+				}
+				rentals, err := rentalRepo.List()
+				if err != nil {
+					printError(err)
+					return
+				}
+
+				for _, r := range rentals {
+					fmt.Println(r.ID, "item: ", r.ItemID, "customer: ", r.CustomerID)
+				}
+			default:
+				fmt.Println("unknown command: ", fields[0])
+			}
+		}()
 
 	}
 }
@@ -127,5 +136,23 @@ func itemStatusLabel(status domain.ItemStatus) string {
 		return "rented"
 	default:
 		panic(fmt.Sprintf("invariant violated: unknown item stats %q", status))
+	}
+}
+
+func printError(err error) {
+	switch {
+	case errors.Is(err, domain.ErrItemAlreadyRented):
+		fmt.Println("item already rented")
+	case errors.Is(err, domain.ErrEntityNotFound):
+		fmt.Println("not found - check ID")
+	case errors.Is(err, domain.ErrRentalClosed):
+		fmt.Println("rental already closed")
+	default:
+		var ve domain.ValidationError
+		if errors.As(err, &ve) {
+			fmt.Println("validation error:", ve.Error())
+		} else {
+			fmt.Println("error: ", err)
+		}
 	}
 }
